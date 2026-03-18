0f6cca854b81b78ed30799ee6d63eb9d4d9b2019
matheusdamacena593 <mateusdamacena593@gmail.com>
2026-03-17 23:33:18 -0300

    feat: adicionando authorize nos controllers e modificando o swagger para permitir envio de token nas requisições

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -1,10 +1,12 @@
-
 using CashFlow.Api.Filters;
 using CashFlow.Api.Middleware;
 using CashFlow.Application;
 using CashFlow.Infrastructure;
 using CashFlow.Infrastructure.Migrations;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
+using System.Text;
 
 namespace CashFlow.Api
 {
@@ -16,12 +18,52 @@ namespace CashFlow.Api
 
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(config =>
+            {
+                config.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Description = @"Informe o token JWT no formato: Bearer {seu_token}. 
+                                    Exemplo: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
+                    In = ParameterLocation.Header,
+                    Scheme = "Bearer",
+                    Type = SecuritySchemeType.ApiKey,
+                });
+
+                config.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                            }
+                        },
+                    }
+                });
+            });
 
             builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
 
             builder.Services.AddInfrastruture(builder.Configuration);
             builder.Services.AddApplication();
+
+            var signingKey = builder.Configuration.GetValue<string>("Settings:Jwt:SigningKey");
+
+            builder.Services.AddAuthentication(config =>
+            {
+                config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(config =>
+            {
+                config.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = new TimeSpan(0),
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey!))
+                };
+            });
 
             var app = builder.Build();
 
@@ -35,6 +77,7 @@ namespace CashFlow.Api
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllers();
